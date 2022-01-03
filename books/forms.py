1555b3ae2de79c405e81072f7afcebe31641a167
Lila <djyasin.ly@gmail.com>
2022-01-03 12:34:51 -0500

    Added forms.

---
+++ books/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Book, Category, User
+from django.contrib.auth.forms import UserCreationForm
+
+class UserForm(UserCreationForm):
+    name = forms.CharField(max_length=100, help_text='Name')
+    email = forms.EmailField(max_length=100, help_text='Email Address')
+
+    class Meta:
+        model = User
+        fields =  ["username", "password", "email"]
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ["title", "author", "description", "book_url", "book_image_url", "created_at", "favorited_by", "category",]
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ["name",]
